d47ea65294f89af4121d4225eb9278aa4492c0ed
Handan <handan.hu@wisc.edu>
2025-06-07 20:38:20 -0500

    feat: post api

--- infra/api.ts
+++ infra/api.ts
@@ -1,8 +1,15 @@
-import { bucket } from "./storage";
+import { table } from "./storage";
 
-export const api = new sst.aws.ApiGatewayV2("Api");
-
-api.route("GET /", {
-  link: [bucket],
-  handler: "packages/functions/src/api.handler",
+// Create the API
+export const api = new sst.aws.ApiGatewayV2("Api", {
+  transform: {
+    route: {
+      handler: {
+        link: [table],
+      },
+    }
+  }
 });
+
+// create a note
+api.route("POST /notes", "packages/functions/src/create.main");
