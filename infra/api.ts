320bdd3f30f368c55c3a80541a6d3d1675fe6730
Handan <handan.hu@wisc.edu>
2025-06-08 17:28:36 -0500

    feat: get notes

--- infra/api.ts
+++ infra/api.ts
@@ -13,3 +13,9 @@ export const api = new sst.aws.ApiGatewayV2("Api", {
 
 // create a note
 api.route("POST /notes", "packages/functions/src/create.main");
+
+// get a note
+api.route("GET /notes/{id}", "packages/functions/src/get.main");
+
+// get all notes of the us
+api.route("GET /notes", "packages/functions/src/list.main");
